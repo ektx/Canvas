d625529c77de1b96cd9893ab2a5fc3603fd1ce03
ektx <530675800@qq.com>
2016-08-02 17:08:23 +0800

    更新

---
+++ Echarts/js/getMapJson.js
@@ -0,0 +1,265 @@
+
+function getMapJson (optionObj) {
+	var url = optionObj.url;
+	var regName = optionObj.regName;
+	var id = optionObj.id;
+	var mapPath = optionObj.mapPath || '../map/';
+	var mapRoute = optionObj.route || false;
+
+	if (!mapRoute) {
+		$.ajax({
+			url: mapPath+'mapRoute.json',
+			type: 'get',
+			dataType: 'json'
+		})
+		.done(function(route) {
+			console.log(route);
+			url = mapPath + route[regName];
+			getMap(url, regName, id)
+		})
+		.fail(function(err) {
+			console.error('Not Find mapPath')
+		})
+		
+	}
+
+}
+
+
+function getMap(url, regName, id) {
+	var colorArr = ['#f44336','#E91E63','#9C27B0','#673AB7','#3F51B5','#2196F3','#03A9F4','#00BCD4','#009688','#4CAF50','#8BC34A','#FF9800'];
+	var colorLen = colorArr.length;
+	
+	var myEChart = echarts.init(document.getElementById(id));
+
+	var resetMapInfo = function(json) {
+		console.log(json)
+
+		// 基于准备好的 dom, 初始化 echarts 实例
+		echarts.registerMap(regName, json)
+		var idata = [];
+
+		// 随机颜色
+		for (var i = 0; i < json.features.length; i++) {
+			idata[i] = {
+				name : json.features[i].properties.name,
+				itemStyle: {
+					normal: {
+						areaColor: colorArr[parseInt(Math.random() * colorLen)]
+					}
+				}
+			}
+		}
+
+		return idata;
+		
+	};
+
+	var getInfoFromUrl = function (_url, _name, _type) {
+		$.get(_url).done(function(data) {
+
+			echarts.registerMap(_name, data);
+
+			if (!_type) {
+
+				if (!window.MYECHARTBACK) {
+					MYECHARTBACK = [
+						{
+							name: _name,
+							url: url
+						}
+					]
+				} else {
+					MYECHARTBACK.push({
+						name: _name,
+						url: url
+					})
+				}
+
+				
+			} else {
+				MYECHARTBACK.pop()
+			}
+
+			// back btn status 
+			if (MYECHARTBACK.length > 0) {
+				$('.back-map-btns button:first-child').removeAttr('disabled');
+				
+			} else {
+				$('.back-map-btns button:first-child').attr('disabled', 'disabled');
+
+			}
+
+			// echarts.registerMap(_name.toLocaleLowerCase(), data);
+			
+			url = _url;
+			
+			// 指定图表的配置和数据
+			option = {
+			    series: [
+			        {
+			            name: _name,
+			            // center: data.features[0].properties.cp,
+			            zoom: 1,
+			            mapType: _name.toLocaleLowerCase(),
+			            data: resetMapInfo(data)
+			        }
+			    ]
+			};
+
+			myEChart.setOption(option);
+		})
+		.fail(function(err) {
+			console.log(err)
+		})
+	};
+
+	
+	/*
+		事件处理
+		---------------------------------------
+	*/
+	$(window).resize(function() {
+		myEChart.resize();
+		console.log(url)
+	})
+
+	// 追加返回操作HTML
+	$('#'+id).append(generateBackMod);
+
+	// 返回操作事件
+	$('#'+id).on('click', 'button',  function(e) {
+		var _ = $(this);
+		var _type = _.attr('data-type');
+
+		if (_type === 'back') {
+			var _backInfo = MYECHARTBACK[MYECHARTBACK.length -1];
+			var _menu = $(this).next();
+
+			getInfoFromUrl(_backInfo.url, _backInfo.name, _type);
+			if (_menu.hasClass('current')) _menu.click();
+		}
+		// menu btn
+		else {
+			if(window.MYECHARTBACK && MYECHARTBACK.length > 0) {
+				var listBox = $('.back-map-menu');
+				var listLen = listBox.find('li').length;
+
+				if (listLen !== MYECHARTBACK.length) {
+					console.log(listLen);
+					var h = '';
+
+					for (var i = 0; i < MYECHARTBACK.length; i++) {
+						h += '<li>'+ MYECHARTBACK[i].name + '</li>'
+					}
+
+					listBox.html(h)
+				}
+
+				$(this).toggleClass('current');
+				listBox.toggle();
+			}
+		}
+	})
+	.on('click', '.back-map-menu li', function() {
+		var _index = $(this).index();
+
+		getInfoFromUrl( MYECHARTBACK[_index].url, MYECHARTBACK[_index].name, 'back');
+
+		console.log(_index,  MYECHARTBACK[_index] );
+		MYECHARTBACK.splice(_index, MYECHARTBACK.length - _index, '');
+
+		console.log(MYECHARTBACK)
+
+		$(this).parents('ul.back-map-menu').hide()
+		.prev().find('button:last-child').toggleClass('current');
+	});
+
+
+
+	myEChart.showLoading();
+
+	$.ajax({
+		url: url,
+		type: 'get',
+		dataType: 'json'
+	})
+	.done(function(mapJson) {
+		
+		myEChart.hideLoading();
+
+		// 指定图表的配置和数据
+		var option = {
+		    tooltip: {
+		        trigger: 'item',
+		        formatter: '{b}'
+		    },
+		    roam: false,
+		    scaleLimit: {
+		    	min: .5
+		    },
+		    series: [
+		        {
+		            name: regName,
+		            type: 'map',
+		            mapType: regName,
+		            selectedMode : 'single',
+		            animationDurationUpdate: 1000,
+		            label: {
+		                normal: {
+		                    show: true
+		                },
+		                emphasis: {
+		                    show: true,
+		                    textStyle: {
+		                    	color: '#c30'
+		                    }
+		                }
+		            },
+		            itemStyle: {
+		            	emphasis: {
+		            		areaColor: '#FFEB3B'
+		            	}
+		            },
+		            data: resetMapInfo(mapJson)
+		        }
+		    ]
+		};
+
+		myEChart.setOption(option);
+
+
+		myEChart.on('click', function(params) {
+
+			console.log(params)
+			// params.seriesName = params.name.
+
+			var _url = url.replace('.json', '/'+params.name+'.json');
+
+			var _menu = $('.back-map-btns button:last-child');
+
+			if (_menu.hasClass('current')) _menu.click();
+
+			// 更新数据
+			getInfoFromUrl(_url, url.substring(url.lastIndexOf('/')+ 1, url.length - 5))
+			// getMap(_url, params.name.toLocaleLowerCase(), id)
+
+		})
+
+		
+	})
+	.error(function(err) {
+		myEChart.hideLoading();
+
+		console.log('没有找到您要的地图信息！')
+	})
+}
+
+
+function generateBackMod() {
+	var html = '<div class="back-map-mod map-right">';
+	html += '<div class="back-map-btns"><button data-type="back" disabled="disabled">Back</button><button>Menu</button></div>';
+	html += '<ul class="back-map-menu"></ul>';
+
+	return html;
+}
--- Echarts/js/iecharts.js
+++ Echarts/js/iecharts.js
@@ -1,4 +1,7 @@
 /*
+	iEcharts v0.7.3
+
+
 	获取平均分布图片
 	--------------------------------------------
 	@id: 生成的元素
@@ -413,7 +416,7 @@ function getLineCharts(id, options, version) {
 	统一处理echarts 2 或 3的转换
 	@id 元素
 	@version 指定版本，默认使用最后调用的echarts
-	--------------------------------------------
+	-------------------------------------------------
 	zwl <530675800@qq.com>             2016.7.14
 */
 function getVersionEC (id, version) {
@@ -427,3 +430,616 @@ function getVersionEC (id, version) {
 
 	return myEchart;
 }
+
+
+
+/*
+	堆叠条形图
+	Eg: 在 div 为 echart 中生成以下内容
+	@id 元素
+	@options 传参
+	@version 指定版本，默认使用最后调用的echarts
+	-----------------------------------------------------
+	<div id="echart"></div>
+
+	var options = {
+		x: {
+			// x 轴上数据
+			data: ['周一','周二','周三','周四','周五','周六','周日', '其它'],
+			// 底线是否显示
+			line: false,
+			// x轴文字颜色
+			color: '#f90'
+		},
+		y: {
+			// 底线是否显示
+			line: false
+		},
+		// 是否使用堆叠 
+		stack: true,
+		// 提示显示格式
+		formatter: '{a} - {b} - {c}',
+		// 图表显示区域,格式: [top, left, right, bottom, 是否显示x值]
+		grid: [0, "0%", 0, "0%", 1],
+		// 线宽
+		barWidth: 10,
+		// 数据 [必填]
+		data: [
+			{
+				name: '直接访问',
+				color: '#09f',
+				data: [320, 302, 301, 334, 390, 330, 320]
+			},
+			{
+	            name: '邮件营销',
+	            color: '#06f',
+	            data: [120, 132, 101, 134, 90, 230, 210]
+	        },
+	        {
+	            name: '联盟广告',
+	            color: '#f90',
+	            data: [220, 182, 191, 234, 290, 330, 310]
+	        },
+	        {
+	            name: '视频广告',
+	            color: '#00f',
+	            data: [150, 212, 201, 154, 190, 330, 410]
+	        },
+	        {
+	            name: '搜索引擎',
+	            color: '#ff0',
+	            data: [820, 832, 901, 934, 1290, 1330, 1320]
+	        }
+		]
+	}
+
+	stackedColumnBar('echart', option)
+	-----------------------------------------------------
+	zwl <530675800@qq.com>                      2016.7.27
+*/
+function stackedColumnBar (id, options, version) {
+
+	var myEcharts = getVersionEC (id, version);
+	var grid = options.grid || [0, 0, 0, 0, 1];
+	var newData = [];
+
+	var getDef = function(val, def) {
+		var result = false;
+		if (val === undefined) {
+			result = def
+		} else {
+			result = val
+		}
+
+		return result
+	};
+
+	for (var i = 0; i < options.data.length; i++) {
+		newData[i] = {
+			name: options.data[i].name,
+			data: options.data[i].data,
+			type: 'bar',
+			stack: !options.stack ? null :'newData' ,
+			barWidth: options.barWidth,
+			label: {
+				normal: {
+					show: false
+				}
+			},
+			itemStyle: {
+				normal: {
+					color: options.data[i].color
+				}
+			}
+		}
+	}
+
+	options.data = newData
+
+	var option = {
+	    tooltip : {
+	        trigger: 'axis',
+	        axisPointer : {      
+	            type : 'shadow'      
+	        },
+	        formatter: options.formatter
+	    },
+	    grid: {
+	    	top: grid[0],
+	        left: grid[1],
+	        right: grid[2],
+	        bottom: grid[3],
+	        containLabel: grid[4]
+	    },
+	    xAxis:  {
+	        type: 'category',
+	        axisTick: {
+	       		show: false
+	        },
+	        axisLine: {
+	    		show:  getDef(options.x.line, true)
+	        },
+	        data: options.x.data
+	        
+	    },
+	    yAxis: {
+	        type: 'value',
+	        axisLine: {
+	    		show: getDef(options.y.line, true)   
+	        },
+	        axisTick: {
+	       		show: false
+	        },
+	        axisLabel: {
+	       		show: false
+	        },
+	        splitLine: {
+	        	show: false
+	        }
+	    },
+	    axisLabel: {
+	    	textStyle: {
+	    		color: options.x.color 
+	    	}
+	    },
+	    series: options.data
+	};
+
+	myEcharts.setOption(option);
+}
+
+/*
+
+		options = {
+			// 简化了 legend 输入方案
+			legend: {
+				itemHeight: 10,
+				itemWidth: 10,
+				top: -2,
+				icon: 'rect',
+				textStyle: {
+					color: '#666'
+				}
+			},
+			xAxis: {
+		        type: 'category',
+		        boundaryGap: false,
+		        splitLine: {
+					show: true,
+					lineStyle: {
+						color: '#2f3033'
+					}
+				},
+		        data:['1','2','3','4','5','6','7', '8', '9', '10', '11', '12']
+			},
+			yAxis: {
+				type: 'value',
+				axisTick: {
+		       		show: false
+		        },
+				splitLine: {
+					show: false
+				},
+				axisLine: {
+					show: false
+				}
+			},
+			lineSmooth: true,
+            showSymbol: false,
+			grid: [ '28px', '20px', '4%', '3%', 1 ],
+			// 简化数据
+			series: [
+			        {
+			            name:'大型企业',
+			            color: '#2b8ac4',
+			            data:[11, 11, 15, 13, 12, 13, 10, 15, 13, 12, 13, 10],
+			        },
+			        {
+			            name:'中型企业',
+			            color: '#f79601',
+			            data:[1, 11, 1, 1, 12, 3, 0, 1, 1, 12, 3, 0],
+			        },
+			        {
+			            name:'小型企业',
+			            color: '#d8422a',
+			            data:[1, 3, 2, 6, 4, 2, 1, 12, 5, 4, 2, 6],
+
+			        },
+			        {
+			            name:'微型企业',
+			            color: '#52ff62',
+			            data:[1, 2, 2, 5, 3, 2, 0, 2, 5, 3, 2, 0],
+
+			        }
+			    ]
+		}
+
+		makeLineCharts('company-charts', options, e3)
+*/
+function makeLineCharts(id, options) {
+
+	myEchart = e3.init(document.getElementById(id));
+	options.legend.data = []
+
+	options.series.forEach(function(value) {
+
+		if ( options.lineSmooth ) {
+			value.smooth = true
+		}
+
+		if ( !options.showSymbol ) {
+			value.showSymbol= false
+		}
+
+		value.type = 'line';
+
+		options.legend.data.push({
+			name: value.name, 
+			icon: options.legend.icon 
+		});
+
+		value.itemStyle = {
+				normal: {
+					color : value.color
+				}
+		};
+
+	});
+
+
+	option = { 
+		tooltip : options.tooltip,
+		grid: {
+			top: options.grid[0],
+			left: options.grid[1],
+			right: options.grid[2],
+			bottom: options.grid[3],
+			containLabel: options.grid[4]
+		},
+	    legend: options.legend,
+	    xAxis:  options.xAxis,
+	    yAxis: options.yAxis,
+	    series: options.series
+	};
+
+	myEchart.setOption(option);
+};
+
+
+//仪表盘
+function makeGaugeCharts(id, options) {
+	myEchart = e3.init(document.getElementById(id));
+
+	var dfop = {
+		backgroundColor: '#1b1b1b',
+		tooltip : {
+			formatter: "{a} <br/>{c} {b}"
+		},
+		toolbox: {
+			show : false,
+			feature : {
+				mark : {show: true},
+				restore : {show: true},
+				saveAsImage : {show: true}
+			}
+		},
+		series : [
+			{
+				name:'速度',
+				type:'gauge',
+				min:0,
+				max:220,
+				splitNumber:11,
+				center : ['50%', '58%'],    // 默认全局居中
+				radius: '100%',
+				axisLine: {            // 坐标轴线
+					lineStyle: {       // 属性lineStyle控制线条样式
+						color: [[0.09, 'lime'],[0.82, '#1e90ff'],[1, '#ff4500']],
+						width: 2,
+						shadowColor : '#fff', //默认透明
+						shadowBlur: 0
+					}
+				},
+				axisLabel: {            // 坐标轴小标记
+					textStyle: {       // 属性lineStyle控制线条样式
+						fontSize: 12,
+						fontWeight: 'bolder',
+						color: '#fff',
+						shadowColor : '#fff', //默认透明
+						shadowBlur: 0
+					}
+				},
+				axisTick: {            // 坐标轴小标记
+					length :9,        // 属性length控制线长
+					lineStyle: {       // 属性lineStyle控制线条样式
+						color: 'auto',
+						shadowColor : '#fff', //默认透明
+						shadowBlur: 0
+					}
+				},
+				splitLine: {           // 分隔线
+					length :15,         // 属性length控制线长
+					lineStyle: {       // 属性lineStyle（详见lineStyle）控制线条样式
+						width: 2,
+						color: '#fff',
+						shadowColor : '#fff', //默认透明
+						shadowBlur: 0
+					}
+				},
+				pointer: {           // 分隔线
+					shadowColor : '#fff', //默认透明
+					shadowBlur: 5
+				},
+				title : {
+					textStyle: {       // 其余属性默认使用全局文本样式，详见TEXTSTYLE
+						fontWeight: 'bolder',
+						fontSize: 14,
+						fontStyle: 'italic',
+						color: '#fff',
+						shadowColor : '#fff', //默认透明
+						shadowBlur: 0
+					}
+				},
+				detail : {
+					backgroundColor: 'rgba(30,144,255,0.8)',
+					borderWidth: 1,
+					borderColor: '#fff',
+					shadowColor : '#fff', //默认透明
+					shadowBlur: 2,
+					offsetCenter: [0, '50%'],       // x, y，单位px
+					textStyle: {       // 其余属性默认使用全局文本样式，详见TEXTSTYLE
+						fontWeight: 'bolder',
+						color: '#fff'
+					}
+				},
+				data:[{value: 40, name: '全社会用电量'}]
+			},
+			{
+				name:'消费价格总指数',
+				type:'gauge',
+				center : ['20%', '68%'],    // 默认全局居中
+				radius : '68%',
+				min:0,
+				max:5,
+				endAngle:45,
+				splitNumber: 5,
+				axisLine: {            // 坐标轴线
+					lineStyle: {       // 属性lineStyle控制线条样式
+						color: [[0.29, 'lime'],[0.86, '#1e90ff'],[1, '#ff4500']],
+						width: 1,
+						shadowColor : '#fff', //默认透明
+						shadowBlur: 0
+					}
+				},
+				axisLabel: {            // 坐标轴小标记
+					textStyle: {       // 属性lineStyle控制线条样式
+						fontWeight: 'bolder',
+						color: '#fff'
+					}
+				},
+				axisTick: {            // 坐标轴小标记
+					length :8,        // 属性length控制线长
+					lineStyle: {       // 属性lineStyle控制线条样式
+						color: 'auto',
+						shadowColor : '#fff', //默认透明
+						shadowBlur: 0
+					}
+				},
+				splitLine: {           // 分隔线
+					length : 15,         // 属性length控制线长
+					lineStyle: {       // 属性lineStyle（详见lineStyle）控制线条样式
+						width: 2,
+						color: '#fff'
+					}
+				},
+				pointer: {
+					width:5,
+					shadowColor : '#fff', //默认透明
+					shadowBlur: 5
+				},
+				title : {
+					show: false,
+					offsetCenter: [0, '-30%'],       // x, y，单位px
+					textStyle: {       // 其余属性默认使用全局文本样式，详见TEXTSTYLE
+						fontWeight: 'bolder',
+						fontStyle: 'italic',
+						color: '#fff',
+						shadowColor : '#fff', //默认透明
+						shadowBlur: 0
+					}
+				},
+				detail : {
+					//backgroundColor: 'rgba(30,144,255,0.8)',
+					// borderWidth: 1,
+					borderColor: '#fff',
+					shadowColor : '#fff', //默认透明
+					shadowBlur: 5,
+					width: 80,
+					height:30,
+					offsetCenter: [25, '20%'],       // x, y，单位px
+					textStyle: {       // 其余属性默认使用全局文本样式，详见TEXTSTYLE
+						fontWeight: 'bolder',
+						color: '#fff',
+						fontSize: 20
+					}
+				},
+				data:[{value: 1.7, name: ''}]
+			},
+			{
+				name:'城镇',
+				type:'gauge',
+				center : ['80%', '62%'],    // 默认全局居中
+				radius : '60%',
+				min:0,
+				max:2,
+				startAngle:135,
+				endAngle:45,
+				splitNumber:2,
+				axisLine: {            // 坐标轴线
+					lineStyle: {       // 属性lineStyle控制线条样式
+						color: [[0.2, 'lime'],[0.8, '#1e90ff'],[1, '#ff4500']],
+						width: 1
+					}
+				},
+				axisTick: {            // 坐标轴小标记
+					length :12,        // 属性length控制线长
+					lineStyle: {       // 属性lineStyle控制线条样式
+						color: 'auto',
+						shadowColor : '#fff', //默认透明
+						shadowBlur: 0
+					}
+				},
+				axisLabel: {
+					textStyle: {       // 属性lineStyle控制线条样式
+						color: '#fff',
+						shadowColor : '#fff', //默认透明
+						shadowBlur: 0
+					},
+					formatter:function(v){
+						switch (v + '') {
+							case '0' : return '低';
+							case '1' : return '城镇';
+							case '2' : return '高';
+						}
+					}
+				},
+				splitLine: {           // 分隔线
+					length :15,         // 属性length控制线长
+					lineStyle: {       // 属性lineStyle（详见lineStyle）控制线条样式
+						width: 2,
+						color: '#fff',
+						shadowColor : '#fff', //默认透明
+						shadowBlur: 0
+					}
+				},
+				pointer: {
+					width:2,
+					shadowColor : '#fff', //默认透明
+					shadowBlur: 5
+				},
+				title : {
+					show: false
+				},
+				detail : {
+					show: false
+				},
+				data:[{value: 3, name: '元'}]
+			},
+			{
+				name:'农村',
+				type:'gauge',
+				center : ['80%', '62%'],    // 默认全局居中
+				radius : '60%',
+				min:0,
+				max:2,
+				startAngle:315,
+				endAngle:225,
+				splitNumber:2,
+				axisLine: {            // 坐标轴线
+					lineStyle: {       // 属性lineStyle控制线条样式
+						color: [[0.2, 'lime'],[0.8, '#1e90ff'],[1, '#ff4500']],
+						width: 1
+					}
+				},
+				axisTick: {            // 坐标轴小标记
+					show: false
+				},
+				axisLabel: {
+					textStyle: {       // 属性lineStyle控制线条样式
+						fontSize: 12,
+						color: '#fff',
+						shadowColor : '#fff', //默认透明
+						shadowBlur: 0
+					},
+					formatter:function(v){
+						switch (v + '') {
+							case '0' : return '低';
+							case '1' : return '农村';
+							case '2' : return '高';
+						}
+					}
+				},
+				splitLine: {           // 分隔线
+					length :15,         // 属性length控制线长
+					lineStyle: {       // 属性lineStyle（详见lineStyle）控制线条样式
+						width: 2,
+						color: '#fff',
+						shadowColor : '#fff', //默认透明
+						shadowBlur: 0
+					}
+				},
+				pointer: {
+					width:2,
+					shadowColor : '#fff', //默认透明
+					shadowBlur: 5
+				},
+				title : {
+					show: false
+				},
+				detail : {
+					show: false
+				},
+				data:[{value: 0.5, name: '元'}]
+			}
+		]
+	}
+	
+	$.extend(dfop,options);
+	for(var i=0; i<options.seriess.length;i++){
+		$.extend(dfop.series[i],options.seriess[i]);
+	};
+	myEchart.setOption(dfop);
+};
+
+
+/*双饼图*/
+function makePieCharts(id,options){
+	myEchart = e3.init(document.getElementById(id));
+
+	var dfop = {
+		tooltip: {
+			trigger: 'item',
+			formatter: "{b}: {c} ({d}%)"
+		},
+		legend: {
+			show: false,
+			orient: 'vertical',
+			x: 'left',
+			data:[]
+		},
+		color: [],
+		series: [
+			{
+				name:'访问来源',
+				type:'pie',
+				radius: ['30%', '50%'],
+				avoidLabelOverlap: false,
+				label: {
+					normal: {
+						show: true,
+						position: 'outside',
+						textStyle:{
+							fontSize: '14'
+						}
+					},
+					labelLine: {
+						normal: {
+							show: true
+						}
+					},
+					emphasis: {
+						show: true,
+						textStyle: {
+							fontSize: '16'
+						}
+					}
+				},
+				data:[]
+			}
+		]
+	};
+	$.extend(dfop,options);
+	dfop.series[0].data = options.data;
+
+	myEchart.setOption(dfop);
+};
